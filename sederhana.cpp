75df584616bf64ce0f0de8405e1158641febcede
Alasi <tomysatriaalasi@live.com>
2023-03-13 21:24:54 +0700

    Create sederhana.cpp

---
+++ sederhana.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int angka;
+    cout <<"Menampilkan sebuah Angka \n";
+    cout<<"isi sebuah angka :";https://github.com/tomylive/cpp
+    cin>>angka;
+    cout<<"Angka adalah : "<< angka*angka*angka<<"\n";
+    return 0;  // ini tujuannya mengambalikan aplikasi atau reset aplikasi
+}
+
+// cari dan jelaskan serta berikan contoh penerapan bahasa pemrograman berorientasi objek
